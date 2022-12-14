3d81cf4ddac67f04346a11c844b5fa538bfb5a24
Víctor Oliva <olivarra1@gmail.com>
2022-12-14 07:04:17 +0100

    solution to day14 part1

---
+++ src/solutions/day14.rs
@@ -0,0 +1,160 @@
+use itertools::Itertools;
+use ndarray::{Array, Array2};
+
+use super::Solver;
+use core::panic;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+pub struct Problem;
+
+pub struct RockPath {
+    path: Vec<(usize, usize)>,
+}
+
+impl FromStr for RockPath {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path = s
+            .split(" -> ")
+            .map(|substr| {
+                let coords: (usize, usize) = substr
+                    .split(",")
+                    .map(|x| x.parse().unwrap())
+                    .collect_tuple()
+                    .unwrap();
+                coords
+            })
+            .collect_vec();
+
+        Ok(RockPath { path })
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum CaveElement {
+    Air,
+    Rock,
+    Sand,
+}
+
+fn put_rocks(
+    dest: &mut Array2<CaveElement>,
+    from: (usize, usize),
+    to: (usize, usize),
+    x_offset: usize,
+) {
+    if from.0 == to.0 {
+        let min = from.1.min(to.1);
+        let max = from.1.max(to.1);
+        let x = from.0 - x_offset;
+        for y in min..(max + 1) {
+            dest[[y, x]] = CaveElement::Rock
+        }
+    } else if from.1 == to.1 {
+        let min = from.0.min(to.0) - x_offset;
+        let max = from.0.max(to.0) - x_offset;
+        let y = from.1;
+        for x in min..(max + 1) {
+            dest[[y, x]] = CaveElement::Rock
+        }
+    } else {
+        panic!("diagonal line?! {:?} {:?}", from, to);
+    }
+}
+
+fn array_from_path(rocks: &Vec<RockPath>) -> (Array2<CaveElement>, usize) {
+    let max_y = rocks
+        .iter()
+        .map(|x| x.path.iter().map(|(_, y)| y).max().unwrap())
+        .max()
+        .unwrap();
+    let min_x = rocks
+        .iter()
+        .map(|r| r.path.iter().map(|(x, _)| x).min().unwrap())
+        .min()
+        .unwrap();
+    let max_x = rocks
+        .iter()
+        .map(|r| r.path.iter().map(|(x, _)| x).max().unwrap())
+        .max()
+        .unwrap();
+    let start_point = 500 - min_x;
+
+    let mut res: Array2<CaveElement> =
+        Array2::from_elem((max_y + 1, max_x - min_x + 1), CaveElement::Air);
+
+    for r in rocks {
+        for (from, to) in (0..r.path.len()).tuple_windows() {
+            put_rocks(&mut res, r.path[from], r.path[to], *min_x);
+        }
+    }
+
+    (res, start_point)
+}
+
+fn put_sand(dest: &mut Array2<CaveElement>, start: usize) -> bool {
+    let shape = dest.shape().to_owned();
+    let mut x = start;
+    let mut y = 0;
+
+    while y < shape[0] - 1 && x < shape[1] {
+        match dest[[y + 1, x]] {
+            CaveElement::Air => {
+                y += 1;
+            }
+            _ => {
+                if x == 0 {
+                    return false;
+                }
+                if dest[[y + 1, x - 1]] == CaveElement::Air {
+                    y += 1;
+                    x -= 1;
+                } else if x == shape[1] - 1 {
+                    return false;
+                } else if dest[[y + 1, x + 1]] == CaveElement::Air {
+                    y += 1;
+                    x += 1;
+                } else {
+                    dest[[y, x]] = CaveElement::Sand;
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+impl Solver for Problem {
+    type Input = Vec<RockPath>;
+    type Output1 = usize;
+    type Output2 = usize;
+
+    fn read_input(&self, file_reader: BufReader<&File>) -> Self::Input {
+        file_reader
+            .lines()
+            .map(|x| x.unwrap())
+            .map(|line| line.parse())
+            .map(|x| x.unwrap())
+            .collect()
+    }
+
+    fn solve_first(&self, input: &Self::Input) -> Result<Self::Output1, String> {
+        let (mut cave, start) = array_from_path(input);
+
+        let mut units = 0;
+        while put_sand(&mut cave, start) {
+            units += 1;
+        }
+
+        Ok(units)
+    }
+
+    fn solve_second(&self, input: &Self::Input) -> Result<Self::Output2, String> {
+        todo!()
+    }
+}
